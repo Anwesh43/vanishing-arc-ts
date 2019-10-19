7d299209282337129aa5eaa3b79fe7ec46f03685
Anwesh43 <anweshthecool0@gmail.com>
2019-10-19 18:03:53 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -61,3 +61,18 @@ class State {
         }
     }
 }
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(scale * Math.PI)
+    }
+}
