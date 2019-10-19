6341f3675346953f165fc9237e75e91ba9e74433
Anwesh43 <anweshthecool0@gmail.com>
2019-10-19 18:22:26 +0530

    adding Animator class

--- index.ts
+++ index.ts
@@ -125,3 +125,23 @@ class DrawingUtil {
         DrawingUtil.drawVanishingArc(context, size, sc)
     }
 }
+
+class Animator {
+
+    animated : boolean = false
+    interval : number
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
