34e9a1083bc88f1ab8e9f528289bf72f67964bf4
Anwesh43 <anweshthecool0@gmail.com>
2019-10-19 17:57:21 +0530

    adding Stage class and constants

---
+++ index.ts
@@ -0,0 +1,39 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.02
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const delay : number = 30
+const foreColor : string = "#2196F3"
+const backColor: string = "#BDBDBD"
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
